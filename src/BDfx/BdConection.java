deca0bd312db80900dc48fda1ccdec69891d79ef
MarceloVRC <Marcelo.Romero@fundacion-jala.org>
2024-04-10 16:07:44 -0400

    Conexion a la BD

--- src/BDfx/BdConection.java
+++ src/BDfx/BdConection.java
@@ -1,4 +1,25 @@
 package BDfx;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class BdConection {
+    Connection conectar = null;
+    String usuario = "postgres";
+    String password = "password";
+    String bd = "postgres";
+    String ip = "localhost";
+    String puerto = "5432";
+    String cadena = "jdbc:postgresql://"+ip+":"+ puerto +"/"+ bd;
+
+    public Connection establecerConexion(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            conectar = DriverManager.getConnection(cadena, usuario, password);
+            System.out.println("Se conecto correctamente a la Base de Datos");
+        }catch (Exception e){
+            System.out.println("No se logro conectar a la Base de Datos");
+        }
+        return conectar;
+        }
 }
---
+++ src/BDfx/Main.java
@@ -0,0 +1,8 @@
+package BDfx;
+
+public class Main {
+    public static void main(String[] args){
+        BdConection conexion = new BdConection();
+        conexion.establecerConexion();
+    }
+}
